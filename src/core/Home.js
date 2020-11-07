05c39c7d5da5f6271f85ba23646d1c4fb85dcdbc
issam <issam.aboulfadl05@gmail.com>
2020-11-07 15:51:40 +0100

    Dashboard of User

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Layout from './Layout';
+import { isAntiticated } from '../auth/helpers';
 
 const Home = () => {
+  const {
+    user: { name, email, role },
+  } = isAntiticated();
   return (
     <div>
       <Layout
@@ -9,7 +13,32 @@ const Home = () => {
         description="Node react Ecommerce app"
         className="container"
       >
-        <h1>Hello</h1>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">User INFORMATION</h5>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">{name}</li>
+                  <li className="list-group-item">{email}</li>
+                  <li className="list-group-item">{role ? 'admin' : 'user'}</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Purshase HISTORY</h5>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item"></li>
+                  <li className="list-group-item"></li>
+                  <li className="list-group-item"></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </Layout>
     </div>
   );
